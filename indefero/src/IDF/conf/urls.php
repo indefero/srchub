31e6ea2a29d09dffe1b6c7d816889e0bba60beb0
Nathan Adams <adamsna@datanethost.net>
2014-04-04 23:59:17 -0500

    Adding new urls

--- indefero/src/IDF/conf/urls.php
+++ indefero/src/IDF/conf/urls.php
@@ -437,6 +437,11 @@ $ctl[] = array('regex' => '#^/p/([\-\w]+)/admin/$#',
                'model' => 'IDF_Views_Project',
                'method' => 'admin');
 
+$ctl[] = array('regex' => '#^/p/([\-\w]+)/admin/backup/$#',
+    'base' => $base,
+    'model' => 'IDF_Views_Project',
+    'method' => 'backup');
+
 $ctl[] = array('regex' => '#^/p/([\-\w]+)/admin/issues/$#',
                'base' => $base,
                'model' => 'IDF_Views_Project',
